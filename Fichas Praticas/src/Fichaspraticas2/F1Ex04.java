505b967e4623918190bfe7520638b39f079375cf
Radutor <Radutor@yandex.ru>
2025-11-09 15:44:37 +0000

    updated

--- Fichas Praticas/src/Fichaspraticas2/F1Ex04.java
+++ Fichas Praticas/src/Fichaspraticas2/F1Ex04.java
@@ -7,38 +7,39 @@ public class F1Ex04 {
 
         Scanner input = new Scanner(System.in);
 
-        int lugar, pos1 = 1, pos2 = 2, pos3 = 3, pos4 = 4, pos5 = 5, pos6 = 6, pos7 = 7, pos8 = 8, valor1 = 10, valor2 = 8, valor3 = 6, valor4 = 5, valor5 = 4, valor6 = 3, valor7 = 2, valor8 = 1, valor9 = 0, pontuacao;
+        int lugar, pos1 = 1, pos2 = 2, pos3 = 3, pos4 = 4, pos5 = 5, pos6 = 6, pos7 = 7, pos8 = 8, valor1 = 10, valor2 = 8, valor3 = 6, valor4 = 5, valor5 = 4, valor6 = 3, valor7 = 2, valor8 = 1, valor9 = 0, pontuacao = 0, num = 1;
 
         System.out.print("Qual foi a sua posicao? ");
         lugar = input.nextInt();
 
         if (lugar == pos1 ) {
-            pontuacao = valor1;
+            pontuacao = valor1 * num;
             System.out.print("A sua pontuacao e de: " + pontuacao);
         } if (lugar == pos2) {
-            pontuacao = valor2;
+            pontuacao = valor2 * num;
             System.out.print("A sua pontuacao e de: " + pontuacao);
         } if (lugar == pos3) {
-            pontuacao = valor3;
+            pontuacao = valor3 * num;
             System.out.print("A sua pontuacao e de: " + pontuacao);
         } if (lugar == pos4) {
-            pontuacao = valor4;
+            pontuacao = valor4 * num;
             System.out.print("A sua pontuacao e de: " + pontuacao);
         } if (lugar == pos5) {
-            pontuacao = valor5;
+            pontuacao = valor5 * num;
             System.out.print("A sua pontuacao e de: " + pontuacao);
         } if (lugar == pos6) {
-            pontuacao = valor6;
+            pontuacao = valor6 * num;
             System.out.print("A sua pontuacao e de: " + pontuacao);
         } if (lugar == pos7) {
-            pontuacao = valor7;
+            pontuacao = valor7 * num;
             System.out.print("A sua pontuacao e de: " + pontuacao);
         } if (lugar == pos8) {
-            pontuacao = valor8;
+            pontuacao = valor8 * num;
             System.out.print("A sua pontuacao é de: " + pontuacao);
         } if (lugar > pos8)
-            pontuacao = valor9;
+            pontuacao = valor9 * num;
             System.out.print("A sua pontuacao e de: " + pontuacao);
         }
+
     }
-}
+
---
+++ Fichas Praticas/src/Fichaspraticas2/F1Ex05.java
@@ -0,0 +1,12 @@
+package Fichaspraticas2;
+
+import java.util.Scanner;
+
+public class F1Ex05 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        
+
+    }
