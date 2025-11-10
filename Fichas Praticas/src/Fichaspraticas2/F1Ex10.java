57bad512eabcd76a5951ac10f8923e8f0a10ea75
Radutor <Radutor@yandex.ru>
2025-11-10 18:20:16 +0000

    update

---
+++ Fichas Praticas/src/Fichaspraticas2/F1Ex10.java
@@ -0,0 +1,22 @@
+package Fichaspraticas2;
+
+import java.util.Scanner;
+
+public class F1Ex10 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int n1, n2, operacao, resultado, soma;
+
+        System.out.print("Digite o primeiro numero: ");
+        n1 = input.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        n2 = input.nextInt();
+
+        System.out.print("Qual das seguintes operacoes pretende utilizar? ");
+        operacao = input.nextInt();
+
+      
+}
