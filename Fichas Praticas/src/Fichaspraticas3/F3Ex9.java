784b993809213194860997bf9d8212d6914ef745
Radutor <Radutor@yandex.ru>
2025-11-20 11:51:27 +0000

    updated

---
+++ Fichas Praticas/src/Fichaspraticas3/F3Ex9.java
@@ -0,0 +1,13 @@
+package Fichaspraticas3;
+
+import java.util.Scanner;
+
+public class F3Ex9 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int media = 0, contador = 0;
+        
+
+}
