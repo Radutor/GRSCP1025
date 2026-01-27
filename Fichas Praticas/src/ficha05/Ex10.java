f3d1d92bb5f61f1186f34bc128e971c5d096d5e9
Radutor <Radutor@yandex.ru>
2026-01-27 18:24:53 +0000

    update

--- Fichas Praticas/src/ficha05/Ex10.java
+++ Fichas Praticas/src/ficha05/Ex10.java
@@ -1,4 +1,25 @@
 package ficha05;
 
+import java.util.Scanner;
+
 public class Ex10 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[][] matriz = new int[3][5];
+
+        for (int linha = 0; linha < matriz.length; linha++) {
+            for (int coluna = 0; coluna < matriz.length; coluna++) {
+                System.out.println("Insira na matriz[" + linha + "][" + coluna + "]: ");
+                matriz[linha][coluna] = input.nextInt();
+            }
+        }
+
+        int valor
+
+        System.out.println("Insira o valor: ");
+        valor = input.nextInt();
+
+
 }
